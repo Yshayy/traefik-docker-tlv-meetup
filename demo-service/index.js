fc419c6d75d8aaca1bc9ce7d7a72c2a925f8ca99
yshay <yshayy@gmail.com>
2018-03-13 10:46:31 +0200

    update scripts

--- demo-service/index.js
+++ demo-service/index.js
@@ -1,5 +1,16 @@
 const http = require("http");
 
+const msg = process.env["MESSAGE"];
+const flakiness = parseFloat(process.env["FLAKINESS"] || "0");
+let currentFlakiness = 0;
+setInterval(()=> currentFlakiness = Math.random(), 5000);
+
 http.createServer(function (request, response) {
-   response.end(process.env["MESSAGE"] || 'Hello World\n');
+    if (currentFlakiness < flakiness && Math.random() < flakiness){
+        response.statusCode = 500;
+        response.end();
+    }
+    else{
+        response.end(process.env["MESSAGE"] || 'Hello World\n');
+    }
 }).listen(3000);
